a052835eba6777ad1ac92cc2cb33e32d2453ef3d
Vivek Kumar Singh, PhD <47096584+vivek-isen@users.noreply.github.com>
2026-05-08 14:25:30 +0530

    Update compute_MDL.m

--- compute_MDL.m
+++ compute_MDL.m
@@ -1,11 +1,7 @@
 function MDL = compute_MDL(signal,est_signal,p)
 N = length(signal);
 norma = max(abs(signal));
-% error = signal - est_signal;
-% mean squared error loss
 error = signal-est_signal;
 lossf = sum(abs(error))/norma;
-% log-cosh loss
-% lossf = sum(log(cosh(error/norma)));
 MDL = p*log(N)/2 + lossf;
 end
